6dd69eaa74c995aadba78a11e45610e2af4e95e9
Hy <s214773@students.aie.edu.au>
2022-06-29 00:54:56 +0930

    ready

--- (Not) Pong/Main.cpp
+++ (Not) Pong/Main.cpp
@@ -4,31 +4,32 @@
 
 struct Ball
 {
-    float x, y;
-    float speedX, speedY;
-    float radius;
+    float x, y; //position of the ball
+    float speedX, speedY; // speed of the ball
+    float radius; // size of the ball
 
     void Draw()
     {
-        DrawCircle((int)x, (int)y, radius, WHITE);
+        DrawCircle((int)x, (int)y, radius, WHITE); // Draws the ball while positioned in the middle of the screen
     }
 };
 struct Paddle
 {
-    float x, y;
-    float speed;
-    float width, height;
+    float x, y; // position of the paddles
+    float speed; // speed of the paddles
+    float width, height; // size of the paddles
 
     Rectangle GetRect()
     {
-        return Rectangle { x - width / 2 ,y - height / 2, 10, 100 };
+        return Rectangle { x - width / 2 ,y - height / 2, 10, 100 }; 
     } 
 
     void Draw()
     {
-        DrawRectangleRec(GetRect(), WHITE);
+        DrawRectangleRec(GetRect(), WHITE); // draws rectangle as white
     }
 };
+
 int main(int argc, char* argv[])
 {
     // Initialization
@@ -42,7 +43,7 @@ int main(int argc, char* argv[])
 #pragma region Ball
     Ball ball;
     ball.x = GetScreenWidth() / 2.0f; 
-    ball.y = GetScreenHeight() / 2.0f; //inital position of the ball (should be in middle of screen)
+    ball.y = GetScreenHeight() / 2.0f; //inital position of the ball (Positioned in middle of screen)
     ball.speedX = 300;
     ball.speedY = 300; // Inital Speed of Ball on both direction X and Y
     ball.radius = 5; // Size of the pong ball
@@ -51,19 +52,19 @@ int main(int argc, char* argv[])
 #pragma region leftPaddle
     Paddle leftPaddle;
     leftPaddle.x = 50;
-    leftPaddle.y = GetScreenHeight() / 2;
+    leftPaddle.y = GetScreenHeight() / 2; // inital position of left paddle
     leftPaddle.width = 10;
-    leftPaddle.height = 100;
-    leftPaddle.speed = 500;
+    leftPaddle.height = 100; // size of paddle
+    leftPaddle.speed = 500; // speed of the paddle
 #pragma endregion
 
 #pragma region rightPaddle 
     Paddle rightPaddle;
     rightPaddle.x = GetScreenWidth() - 50;
-    rightPaddle.y = GetScreenHeight() / 2;
+    rightPaddle.y = GetScreenHeight() / 2; // inital position of right paddle
     rightPaddle.width = 10;
-    rightPaddle.height = 100;
-    rightPaddle.speed = 500;
+    rightPaddle.height = 100;// size of paddle
+    rightPaddle.speed = 500;// speed of the paddle
 #pragma endregion
 
     const char* scoreText = nullptr;
@@ -73,16 +74,16 @@ int main(int argc, char* argv[])
     {
         deltaTime = GetFrameTime();
         ball.x += ball.speedX * GetFrameTime();
-        ball.y += ball.speedY * GetFrameTime();
+        ball.y += ball.speedY * GetFrameTime(); // gives direction of the ball
         // Update
         //----------------------------------------------------------------------------------
 #pragma region Ball Movement Update
-        if (ball.y < 0)
+        if (ball.y < 0) //if ball hits bottom or top of screen -> goes other direction
         {
             ball.y = 0;
             ball.speedY *= -1;
         }
-        if (ball.y > GetScreenHeight())
+        if (ball.y > GetScreenHeight())//if ball hits top of screen -> goes other direction
         {
             ball.y = GetScreenHeight();
             ball.speedY *= -1;
@@ -109,17 +110,36 @@ int main(int argc, char* argv[])
 
 #pragma endregion
 
-#pragma region BallRectCollusion
-        if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, rightPaddle.GetRect()))
+#pragma region  Paddle Bound
+        if (leftPaddle.y > GetScreenHeight()) //Setting up bounds on left paddle in order to prevent from going out of screen
         {
-            if (ball.speedX < 0)
+            leftPaddle.y = GetScreenHeight();
+        }
+        if (leftPaddle.y < 0)
+        {
+            leftPaddle.y = 0;
+        }
+
+        if (rightPaddle.y > GetScreenHeight())//Setting up bounds on right paddle in order to prevent from going out of screen
+        {
+            rightPaddle.y = GetScreenHeight();
+        }
+        if (rightPaddle.y < 0)
+        {
+            rightPaddle.y = 0;
+        }
+#pragma endreigon
+#pragma region BallRectCollusion
+        if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, rightPaddle.GetRect())) // collusion of ball against right paddle while giving different direction and increase the speed
+        {
+            if (ball.speedX < 0) 
             {
                 ball.speedX *= -1.1f;
                 ball.speedY = (ball.y - rightPaddle.y) / (rightPaddle.height / 2) * -ball.speedX;
             }
             ball.speedX *= -1;
         }
-        if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, leftPaddle.GetRect()))
+        if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, leftPaddle.GetRect()))// collusion of ball against left paddle while giving different direction and increase the speed
         {
             if (ball.speedX > 0)
             {
@@ -131,21 +151,23 @@ int main(int argc, char* argv[])
 #pragma endregion
 
 #pragma region WinCondition
-        if (ball.x < 0)
+        if (ball.x < 0) // if ball hits left side
         {
             scoreText = "Player Right Scored!!!!";
+
         }
-        if (ball.x > GetScreenWidth())
+        if (ball.x > GetScreenWidth()) // if ball hits right side
         {
             scoreText = "Player Left Scored!!!!";
+  
         }
-        if (scoreText && IsKeyPressed(KEY_SPACE))
+        if (scoreText && IsKeyPressed(KEY_SPACE)) // resets the game
         {
+            scoreText = nullptr;
             ball.x = GetScreenWidth() / 2;
             ball.y = GetScreenHeight() / 2;
             ball.speedX = 300;
             ball.speedY = 300;
-            scoreText = nullptr;
         }
 #pragma endregion
 
@@ -162,7 +184,7 @@ int main(int argc, char* argv[])
         if (scoreText)
         {
             int textWidth = MeasureText(scoreText, 60);
-            DrawText(scoreText, GetScreenWidth() /  2 - textWidth / 2, GetScreenHeight() / 2 - 30, 60, WHITE);
+            DrawText(scoreText, GetScreenWidth() /  2 - textWidth / 2, GetScreenHeight() / 2 - 30, 60, WHITE); // draws the text when either side wins
         }
         DrawFPS(0, 0);
         EndDrawing();
--- IntroToCpp_Arrays_Exercises/IntroToCpp_Arrays_Exercises/AIE_06/main.cpp
+++ IntroToCpp_Arrays_Exercises/IntroToCpp_Arrays_Exercises/AIE_06/main.cpp
@@ -1,4 +1,4 @@
-
+#include "raylib.h"
 #include "Application.h"
 
 
